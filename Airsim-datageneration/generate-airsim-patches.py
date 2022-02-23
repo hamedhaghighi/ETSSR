adbc4ae075552a18925bc8b128ddd0195bf0d5d2
Hamad Haghighi <haghig_h@wmgubws05.wmgds.wmg.warwick.ac.uk>
2022-02-23 15:38:39 +0000

    modify airsim data generation

--- Airsim-datageneration/generate-airsim-patches.py
+++ Airsim-datageneration/generate-airsim-patches.py
@@ -1,11 +1,8 @@
 import os
 import numpy as np
 import cv2
-from stereomideval.structures import MatchData
-from stereomideval.dataset import Dataset
-from stereomideval.eval import Eval, Timer
 
-root_dir ='/media/oem/Local Disk/Phd-datasets/iPASSR/datasets_Airsim'
+root_dir ='/home/haghig_h@WMGDS.WMG.WARWICK.AC.UK/Phd_datasets/iPASSR'
 
 def modcrop(img, scale):
     h , w = img.shape[0], img.shape[1]
@@ -16,37 +13,37 @@ scale = 2
 idx_patch = 1
 match_data_list = []
 # Get list of scenes in Milddlebury's stereo training dataset and iterate through them
-image_folders = os.listdir(root_dir)
-for folder in image_folders:
-    
-    image_path = os.path.join(root_dir, folder)
-    left_image = np.load(image_path + '/im0.npy')
-    right_image = np.load(image_path + '/im1.npy')
-    # # Scene data class contains the following data:
-    img_hr_0 = modcrop(left_image, scale)
-    img_hr_1 = modcrop(right_image, scale)
-    img_lr_0 = cv2.resize(img_hr_0.astype('uint8'), (int(0.5 * img_hr_0.shape[1]) , int(0.5 * img_hr_0.shape[0])), interpolation=cv2.INTER_CUBIC)
-    img_lr_1 = cv2.resize(img_hr_1.astype('uint8'), (int(0.5 * img_hr_1.shape[1]), int(0.5 * img_hr_1.shape[0])), interpolation=cv2.INTER_CUBIC)
-    img_lr_0 = img_lr_0.astype('float32')
-    img_lr_1 = img_lr_1.astype('float32')
-    img_lr_0[:,:,3] = cv2.resize(img_hr_0[:,:,3], (int(0.5 * img_hr_0.shape[1]), int(0.5 * img_hr_0.shape[0])))
-    img_lr_1[:,:,3] = cv2.resize(img_hr_1[:,:,3], (int(0.5 * img_hr_1.shape[1]), int(0.5 * img_hr_1.shape[0])))
-    for x_lr in range(2, img_lr_0.shape[0] - 32, 20):
-        for y_lr in range(2, img_lr_0.shape[1] - 92, 20):
-            x_hr = x_lr * scale
-            y_hr = y_lr * scale
-            hr_patch_0 = img_hr_0[x_hr: (x_lr + 30)*scale, y_hr: (y_lr + 90)*scale]
-            hr_patch_1 = img_hr_1[x_hr: (x_lr + 30)*scale, y_hr: (y_lr + 90)*scale]
-            lr_patch_0 = img_lr_0[x_lr: x_lr + 30, y_lr: y_lr + 90]
-            lr_patch_1 = img_lr_1[x_lr: x_lr + 30, y_lr: y_lr + 90]
-            dst_root = os.path.join(root_dir, 'patches_x{:d}/{:06d}'.format(scale, idx_patch)) 
-            os.makedirs(dst_root, exist_ok=True)
-            np.save(dst_root + '/hr0.npy', hr_patch_0)
-            np.save(dst_root + '/hr1.npy', hr_patch_1)
-            np.save(dst_root + '/lr0.npy', lr_patch_0)
-            np.save(dst_root + '/lr1.npy', lr_patch_1)
-            print('writing patch id:', idx_patch)
-            idx_patch = idx_patch + 1
+for env_folder in os.listdir(root_dir):
+    env_path = os.path.join(root_dir, env_folder)
+    for image_folder in os.listdir(env_path):
+        image_path = os.path.join(env_path, image_folder)
+        left_image = np.load(image_path + '/im0.npy')
+        right_image = np.load(image_path + '/im1.npy')
+        # # Scene data class contains the following data:
+        img_hr_0 = modcrop(left_image, scale)
+        img_hr_1 = modcrop(right_image, scale)
+        img_lr_0 = cv2.resize(img_hr_0.astype('uint8'), (int(0.5 * img_hr_0.shape[1]) , int(0.5 * img_hr_0.shape[0])), interpolation=cv2.INTER_CUBIC)
+        img_lr_1 = cv2.resize(img_hr_1.astype('uint8'), (int(0.5 * img_hr_1.shape[1]), int(0.5 * img_hr_1.shape[0])), interpolation=cv2.INTER_CUBIC)
+        img_lr_0 = img_lr_0.astype('float32')
+        img_lr_1 = img_lr_1.astype('float32')
+        img_lr_0[:,:,3] = cv2.resize(img_hr_0[:,:,3], (int(0.5 * img_hr_0.shape[1]), int(0.5 * img_hr_0.shape[0])))
+        img_lr_1[:,:,3] = cv2.resize(img_hr_1[:,:,3], (int(0.5 * img_hr_1.shape[1]), int(0.5 * img_hr_1.shape[0])))
+        for x_lr in range(2, img_lr_0.shape[0] - 32, 20):
+            for y_lr in range(2, img_lr_0.shape[1] - 92, 20):
+                x_hr = x_lr * scale
+                y_hr = y_lr * scale
+                hr_patch_0 = img_hr_0[x_hr: (x_lr + 30)*scale, y_hr: (y_lr + 90)*scale]
+                hr_patch_1 = img_hr_1[x_hr: (x_lr + 30)*scale, y_hr: (y_lr + 90)*scale]
+                lr_patch_0 = img_lr_0[x_lr: x_lr + 30, y_lr: y_lr + 90]
+                lr_patch_1 = img_lr_1[x_lr: x_lr + 30, y_lr: y_lr + 90]
+                dst_root = os.path.join(root_dir, 'patches_x{:d}/{:06d}'.format(scale, idx_patch)) 
+                os.makedirs(dst_root, exist_ok=True)
+                np.save(dst_root + '/hr0.npy', hr_patch_0)
+                np.save(dst_root + '/hr1.npy', hr_patch_1)
+                np.save(dst_root + '/lr0.npy', lr_patch_0)
+                np.save(dst_root + '/lr1.npy', lr_patch_1)
+                print('writing patch id:', idx_patch)
+                idx_patch = idx_patch + 1
     
     # ground_truth_disp_image = scene_data.disp_image
     
--- Airsim-datageneration/hello_car.py
+++ Airsim-datageneration/hello_car.py
@@ -10,13 +10,10 @@ import itertools
 # connect to the AirSim simulator
 client = airsim.CarClient()
 client.confirmConnection()
-client.enableApiControl(True)
+# client.enableApiControl(True)
+# car_controls = airsim.CarControls()
 
-print("API Control enabled: %s" % client.isApiControlEnabled())
-car_controls = airsim.CarControls()
-
-tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_car")
-print ("Saving images to %s" % tmp_dir)
+# print("API Control enabled: %s" % client.isApiControlEnabled())
 
 def NormalizeandRemoveInf(d):
     d_t = np.copy(d)
@@ -26,31 +23,32 @@ def NormalizeandRemoveInf(d):
     d = np.clip(d, 0.0, 1.0)
     return d
 
-try:
-    os.makedirs(tmp_dir)
-except OSError:
-    if not os.path.isdir(tmp_dir):
-        raise
-root_dir = '/media/oem/Local Disk/Phd-datasets/iPASSR/datasets_Airsim'
+environment = 'AirsimNH'
+root_dir = os.path.join('/home/haghig_h@WMGDS.WMG.WARWICK.AC.UK/Phd_datasets/iPASSR' , environment)
+if os.path.isdir(root_dir):
+    start_idx = len(os.listdir(root_dir))
+else:
+    start_idx = 0
 
-for idx in range(10):
+for idx in range(start_idx, start_idx + 10):
     # get state of the car
-    car_state = client.getCarState()
-    print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
+    # car_state = client.getCarState()
+    # print("Speed %d, Gear %d" % (car_state.speed, car_state.gear))
 
-    # go forward
-    car_controls.throttle = 0.5
-    car_controls.steering = 0
-    client.setCarControls(car_controls)
-    print("Go Forward")
-    time.sleep(3)   # let car drive a bit
+    # # go forward
+    # car_controls.throttle = 0.5
+    # car_controls.steering = 0
+    # client.setCarControls(car_controls)
+    # print("Go Forward")
+    # time.sleep(3)   # let car drive a bit
 
-    # apply brakes
-    car_controls.brake = 1
-    client.setCarControls(car_controls)
-    print("Apply brakes")
-    time.sleep(3)   # let car drive a bit
-    car_controls.brake = 0 #remove brake
+    # # apply brakes
+    # car_controls.brake = 1
+    # client.setCarControls(car_controls)
+    # print("Apply brakes")
+    # time.sleep(3)   # let car drive a bit
+    # car_controls.brake = 0 #remove brake
+    time.sleep(5)
 
     requests = []
 
@@ -72,7 +70,7 @@ for idx in range(10):
                 img_d = 0.54 * (response.shape[1]/2) * (1/response)
                 response_list.append(img_d)
             else:
-                img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) #get numpy array
+                img1d = np.frombuffer(response.image_data_uint8, dtype=np.uint8) #get numpy array
                 img_rgb = img1d.reshape(response.height, response.width, 3)
                 response_list.append(img_rgb[:,:,::-1].astype('float32'))
         LandR_list.append(np.concatenate(response_list, axis=-1))
@@ -82,8 +80,9 @@ for idx in range(10):
     np.save(filename, LandR_list[0])
     filename = os.path.join(img_path, 'im1.npy')
     np.save(filename, LandR_list[1])
+    print('image', idx, 'from environemnt ', environment)
 
 #restore to original state
 client.reset()
 
-client.enableApiControl(False)
+# client.enableApiControl(False)
