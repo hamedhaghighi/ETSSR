resolve big mistake in flop computation and add flops to SOTA models

model_zoo/EDSR.py
@@ -7,6 +7,11 @@ from skimage import morphology
 from models.BaseModel import BaseModel
 
 
+def conv_flop(N, in_ch, out_ch, K, bias=False):
+    if bias:
+        return N * (K**2 * in_ch + 1) * out_ch
+    return N * K**2 * in_ch * out_ch
+
 class EDSR(BaseModel):
     def __init__(self, upscale_factor):
         super(EDSR, self).__init__()
@@ -35,11 +40,16 @@ class EDSR(BaseModel):
         x_left = self.one_image_output(x_left)
         x_right = self.one_image_output(x_right)
         return x_left, x_right
-
+    
+    def flop(self, H, W):
+        N = H * W
+        flop = 0
+        flop += conv_flop(N, 3, 256, 3)
 
 class ResB(nn.Module):
     def __init__(self, n_feat):
         super(ResB, self).__init__()
+        self.n_feat = n_feat
         modules_body = []
         for i in range(2):
             modules_body.append(nn.Conv2d(n_feat, n_feat, 3, 1, 1))
@@ -51,18 +61,30 @@ class ResB(nn.Module):
         res += x
         return res
 
+    def flop(self, N):
+        flop = 0
+        flop += 2 * conv_flop(N, self.n_feat, self.n_feat, 3)
+        return flop
 
 class ResidualGroup(nn.Module):
     def __init__(self, n_feat, n_resblocks):
         super(ResidualGroup, self).__init__()
-        modules_body = [
+        self.n_feat = n_feat
+        self.n_resblock = n_resblocks
+        self.modules_body = [
             ResB(n_feat) \
             for _ in range(n_resblocks)]
-        modules_body.append(nn.Conv2d(n_feat, n_feat, 3, 1, 1))
-        self.body = nn.Sequential(*modules_body)
+        self.modules_body.append(nn.Conv2d(n_feat, n_feat, 3, 1, 1))
+        self.body = nn.Sequential(*self.modules_body)
 
     def forward(self, x):
         res = self.body(x)
         res += x
         return res
 
+    def flop(self, N):
+        flop = 0
+        flop += conv_flop(N, 3, 256, 3)
+        flop += self.n_resblock * self.modules_body[0].flop(N)
+        flop += conv_flop(N, self.n_feat, self.n_feat, 3)
+        return flop

model_zoo/PASSRnet.py
@@ -1,3 +1,4 @@
+from turtle import up
 import torch
 import torch.nn as nn
 import numpy as np
@@ -5,10 +6,24 @@ import matplotlib.pyplot as plt
 from skimage import morphology
 from models.BaseModel import BaseModel
 
+
+def conv_flop(N, in_ch, out_ch, K, bias=False):
+    if bias:
+        return N * (K**2 * in_ch + 1) * out_ch
+    return N * K**2 * in_ch * out_ch
+
+def ResB_flops(N, channel):
+    return 2 * conv_flop(N, channel, channel, 3)
+
+
+def ResASPPB_flops(N, channel):
+    return 9 * conv_flop(N, channel, channel, 3) + 3 * conv_flop(N, channel * 3, channel, 3)
+
 class PASSRnet(BaseModel):
     def __init__(self, upscale_factor):
         super(PASSRnet, self).__init__()
         ### feature extraction
+        self.upscale_factor = upscale_factor
         self.init_feature = nn.Sequential(
             nn.Conv2d(3, 64, 3, 1, 1, bias=False),
             nn.LeakyReLU(0.1, inplace=True),
@@ -46,6 +61,26 @@ class PASSRnet(BaseModel):
         out_left = self.one_image_output(x_left, x_right)
         out_right = self.one_image_output(x_right, x_left)
         return out_left, out_right
+
+    def flop(self, H, W):
+        N = H * W
+        flops = 0
+        flops += 2 * conv_flop(N, 3, 64, 3)
+        # ResB
+        flops += 2 * 3 * (ResB_flops(N, 64))
+        # ResASPPB
+        flops += 2 * 2 * (ResASPPB_flops(N, 64))
+        # pam
+        flops += self.pam.flop(H, W)
+        # upscale
+        flops += 4 * (ResB_flops(N, 64))
+        flops += conv_flop(N, 64, 64 * self.upscale_factor ** 2, 1)
+        flops += conv_flop(N * self.upscale_factor ** 2, 64, 3, 3)
+        flops += conv_flop(N * self.upscale_factor ** 2, 3, 3, 3)
+        
+        return flops
+
+
 
 
 class ResB(nn.Module):
@@ -112,6 +147,7 @@ class ResASPPB(nn.Module):
 class PAM(nn.Module):
     def __init__(self, channels):
         super(PAM, self).__init__()
+        self.channels = channels
         self.b1 = nn.Conv2d(channels, channels, 1, 1, 0, bias=True)
         self.b2 = nn.Conv2d(channels, channels, 1, 1, 0, bias=True)
         self.b3 = nn.Conv2d(channels, channels, 1, 1, 0, bias=True)
@@ -119,7 +155,7 @@ class PAM(nn.Module):
         self.rb = ResB(64)
         self.fusion = nn.Conv2d(channels * 2 + 1, channels, 1, 1, 0, bias=True)
 
-    def __call__(self, x_left, x_right, is_training):
+    def forward(self, x_left, x_right, is_training):
         b, c, h, w = x_left.shape
         buffer_left = self.rb(x_left)
         buffer_right = self.rb(x_right)
@@ -155,6 +191,17 @@ class PAM(nn.Module):
         out = self.fusion(torch.cat((buffer, x_left, V_left_to_right), 1))
 
         return out
+
+    def flop(self, H, W):
+        N = H * W
+        flop = 0
+        flop += 2 * ResB_flops(N, 64)
+        flop += 5 * conv_flop(N, self.channels, self.channels, 1, True)
+        flop += 2 * H * (W**2) * self.channels
+        # flop += 2 * H * W
+        flop += H * (W**2) * self.channels
+        flop += conv_flop(N, self.channels * 2 + 1, self.channels, 1, True)
+        return flop
 
 
 def morphologic_process(mask):

model_zoo/RCAN.py
@@ -1,3 +1,4 @@
+from termios import N_MOUSE
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,9 +7,16 @@ import matplotlib.pyplot as plt
 from skimage import morphology
 from models.BaseModel import BaseModel
 
+
+def conv_flop(N, in_ch, out_ch, K, bias=False):
+    if bias:
+        return N * (K**2 * in_ch + 1) * out_ch
+    return N * K**2 * in_ch * out_ch
+
 class RCAN(BaseModel):
     def __init__(self, upscale_factor):
         super(RCAN, self).__init__()
+        self.upscale_factor = upscale_factor
         self.init_feature = nn.Conv2d(3, 64, 3, 1, 1)
         self.RG1 = ResidualGroup(n_feat=64, n_resblocks=20)
         self.RG2 = ResidualGroup(n_feat=64, n_resblocks=20)
@@ -47,12 +55,22 @@ class RCAN(BaseModel):
         x_left = self.one_image_output(x_left)
         x_right = self.one_image_output(x_right)
         return x_left, x_right
+    
+    def flop(self, H, W):
+        N = H * W
+        flop = 0
+        flop += conv_flop(N, 3, 64, 3)
+        flop += 10 * self.RG1.flop(N)
+        flop += conv_flop(N, 64, 64 * self.upscale_factor**2, 1)
+        flop += conv_flop(N * self.upscale_factor**2, 64, 3, 3)
+        return flop
 
 
 ## Channel Attention (CA) Layer
 class CALayer(nn.Module):
     def __init__(self, channel):
         super(CALayer, self).__init__()
+        self.channel = channel
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.conv_du = nn.Sequential(
                 nn.Conv2d(channel, 4, 1, padding=0, bias=True),
@@ -66,16 +84,26 @@ class CALayer(nn.Module):
         y = self.conv_du(y)
         return x * y
 
+    def flop(self, N):
+        flop = 0
+        flop += N * self.channel
+        flop += (self.channel + 1) * 4
+        flop += (4 + 1) * self.channel
+        flop += N * self.channel
+        return flop
+
 
 ## Residual Channel Attention Block (RCAB)
 class RCAB(nn.Module):
     def __init__(self, n_feat):
         super(RCAB, self).__init__()
+        self.n_feat = n_feat
         modules_body = []
         for i in range(2):
             modules_body.append(nn.Conv2d(n_feat, n_feat, 3, 1, 1))
             if i == 0: modules_body.append(nn.LeakyReLU(0.1, inplace=True))
-        modules_body.append(CALayer(n_feat))
+        self.ca_layer = CALayer(n_feat)
+        modules_body.append(self.ca_layer)
         self.body = nn.Sequential(*modules_body)
 
     def forward(self, x):
@@ -83,29 +111,32 @@ class RCAB(nn.Module):
         res += x
         return res
 
+    def flop(self, N):
+        flop = 0
+        flop += 2 * conv_flop(N, self.n_feat, self.n_feat, 3)
+        flop += self.ca_layer.flop()
+        return flop
 
 ## Residual Group (RG)
 class ResidualGroup(nn.Module):
     def __init__(self, n_feat, n_resblocks):
         super(ResidualGroup, self).__init__()
-        modules_body = [
+        self.n_feat = n_feat
+        self.n_resblocks = n_resblocks
+        self.modules_body = [
             RCAB(n_feat) \
             for _ in range(n_resblocks)]
-        modules_body.append(nn.Conv2d(n_feat, n_feat, 3, 1, 1))
-        self.body = nn.Sequential(*modules_body)
+        self.modules_body.append(nn.Conv2d(n_feat, n_feat, 3, 1, 1))
+        self.body = nn.Sequential(*self.modules_body)
 
     def forward(self, x):
         res = self.body(x)
         res += x
         return res
+    
+    def flop(self, N):
+        flop = 0
+        flop += self.n_resblocks * self.modules_body[0].flop(N)
+        flop += conv_flop(N, self.n_feat, self.n_feat, 3)
+        return flop
 
-
-if __name__ == "__main__":
-    net = RCAN(upscale_factor=4).cuda()
-    from thop import profile
-    input = torch.randn(1, 3, 128, 128).cuda()
-    total = sum([param.nelement() for param in net.parameters()])
-    flops, params = profile(net, inputs=(input,))
-    print('   Number of parameters: %.2fM' % (total / 1e6))
-    print('   Number of parameters: %.2fM' % (params/1e6))
-    print('   Number of FLOPs: %.2fG' % (flops*2/1e9))

model_zoo/RDN.py
@@ -7,14 +7,20 @@ from skimage import morphology
 from models.BaseModel import BaseModel
 
 
+def conv_flop(N, in_ch, out_ch, K, bias=False):
+    if bias:
+        return N * (K**2 * in_ch + 1) * out_ch
+    return N * K**2 * in_ch * out_ch
+
 class RDN(BaseModel):
     def __init__(self, upscale_factor):
         super(RDN, self).__init__()
         self.upscale_factor = upscale_factor
         self.init_feature = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.RDG = RDG(G0=64, C=8, G=64, n_RDB=16)
         self.reconstruct = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            RDG(G0=64, C=8, G=64, n_RDB=16),
+            self.RDG,
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1))
         self.upscale = nn.Sequential(
             nn.Conv2d(64, 64 * upscale_factor ** 2, 1, 1, 0, bias=False),
@@ -34,39 +40,59 @@ class RDN(BaseModel):
         x_right = self.one_image_output(x_right)
         return x_left, x_right
 
+    def flop(self, H, W):
+        N = H * W
+        flop = 0 
+        flop += conv_flop(N, 3, 64, 3)
+        flop += 2 * conv_flop(N, 64, 64, 3)
+        flop += self.RDG.flop(N)
+        flop += conv_flop(N, 64, 64 * self.upscale_factor**2, 1)
+        flop += conv_flop(N * self.upscale_factor**2, 64, 3, 3)
+        return flop
+
 
 class one_conv(nn.Module):
     def __init__(self, G0, G):
         super(one_conv, self).__init__()
+        self.G0, self.G = G0, G
         self.conv = nn.Conv2d(G0, G, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU(inplace=True)
     def forward(self, x):
         output = self.relu(self.conv(x))
         return torch.cat((x, output), dim=1)
 
+    def flop(self, N):
+        flop = N * (self.G0 * 9 + 1) * self.G
+        return flop
 
 class RDB(nn.Module):
     def __init__(self, G0, C, G):
         super(RDB, self).__init__()
-        convs = []
+        self.convs = []
         for i in range(C):
-            convs.append(one_conv(G0+i*G, G))
-        self.conv = nn.Sequential(*convs)
+            self.convs.append(one_conv(G0+i*G, G))
+        self.conv = nn.Sequential(*self.convs)
         self.LFF = nn.Conv2d(G0+C*G, G0, kernel_size=1, stride=1, padding=0)
     def forward(self, x):
         out = self.conv(x)
         lff = self.LFF(out)
         return lff + x
 
+    def flop(self, N):
+        flop = 0
+        for cv in self.convs:
+            flop += cv.flop(N)
+        flop += N * (self.G0 + self.C*self.G + 1) * self.G0
+        return flop
 
 class RDG(nn.Module):
     def __init__(self, G0, C, G, n_RDB):
         super(RDG, self).__init__()
         self.n_RDB = n_RDB
-        RDBs = []
+        self.RDBs = []
         for i in range(n_RDB):
-            RDBs.append(RDB(G0, C, G))
-        self.RDB = nn.Sequential(*RDBs)
+            self.RDBs.append(RDB(G0, C, G))
+        self.RDB = nn.Sequential(*self.RDBs)
         self.conv = nn.Conv2d(G0*n_RDB, G0, kernel_size=1, stride=1, padding=0)
 
     def forward(self, x):
@@ -79,13 +105,10 @@ class RDG(nn.Module):
         out = self.conv(out)
         return out
 
+    def flop(self, N):
+        flop = 0
+        flop += len(self.RDBs) * self.RDBs[0].flop(N)
+        flop += N * (self.n_RDB * self.G0 + 1) * self.G0
+        return flop
 
-if __name__ == "__main__":
-    net = RDN(upscale_factor=2).cuda()
-    from thop import profile
-    input = torch.randn(1, 3, 128, 128).cuda()
-    total = sum([param.nelement() for param in net.parameters()])
-    flops, params = profile(net, inputs=(input,))
-    print('   Number of parameters: %.2fM' % (total / 1e6))
-    print('   Number of parameters: %.2fM' % (params/1e6))
-    print('   Number of FLOPs: %.2fG' % (flops*2/1e9))
+

model_zoo/VDSR.py
@@ -3,11 +3,17 @@ import torch.nn as nn
 from math import sqrt
 from models.BaseModel import BaseModel
 
+
+def conv_flop(N, in_ch, out_ch, K, bias=False):
+    if bias:
+        return N * (K**2 * in_ch + 1) * out_ch
+    return N * K**2 * in_ch * out_ch
+
+
 class Conv_ReLU_Block(nn.Module):
     def __init__(self):
         super(Conv_ReLU_Block, self).__init__()
-        self.conv = nn.Conv2d(in_channels=64, out_channels=64,
-                              kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
@@ -18,10 +24,8 @@ class Net(BaseModel):
     def __init__(self):
         super(Net, self).__init__()
         self.residual_layer = self.make_layer(Conv_ReLU_Block, 18)
-        self.input = nn.Conv2d(
-            in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.output = nn.Conv2d(
-            in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.input = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.output = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.ReLU(inplace=True)
 
         for m in self.modules():
@@ -48,4 +52,17 @@ class Net(BaseModel):
         x_left = self.one_image_output(x_left)
         x_right = self.one_image_output(x_right)
         return x_left, x_right
+
     
+    def flop(self, H, W):
+        N = H * W
+        flop = 0
+        # input
+        flop += conv_flop(N, 3, 64, 3, False)
+        # residual
+        flop += 18 * conv_flop(N, 64, 64, 3, False)
+        # output
+        flop += conv_flop(N, 64, 3, 3, False)
+        return flop
+
+

models/SwinTransformer.py
@@ -294,7 +294,7 @@ class RSTB(nn.Module):
         flops = 0
         for block in self.blocks:
             flops += block.flops(H, W)
-        flops += H * W * self.dim * self.dim * 9
+        flops += H * W * (self.dim + 1) * self.dim * 9
 
         return flops
 
@@ -353,10 +353,8 @@ class SwinAttn(nn.Module):
 
         x_size = (x.shape[2], x.shape[3])
         x = x.flatten(2).transpose(1, 2)
-        # x = self.pre_norm(x) 
         for layer in self.layers:
             x = layer(x, x_size)
-        # x = self.norm(x)  # B L C
         B, HW, C = x.shape
         x = x.transpose(1, 2).view(B, C, x_size[0], x_size[1])
         return x
@@ -365,7 +363,6 @@ class SwinAttn(nn.Module):
         flops = 0
         for layer in self.layers:
             flops += layer.flops(H, W)
-        # flops += 2 * H * W * self.embed_dim
         return flops
 
     

models/model.py
@@ -30,7 +30,6 @@ class Net(BaseModel):
             self.condition_feature = nn.Sequential(nn.Conv2d(4, 64, 3, 1, 1, bias=True), nn.LeakyReLU(0.1, inplace=True), nn.Conv2d(64, 64, 3, 1, 1, bias=True))
         self.n_RDB = 3 if 'MDB' in model else 3
         self.deep_feature = RDG(G0=64, C=4, G=24, n_RDB=self.n_RDB, type='P') if 'MDB' in model else RDG(G0=64, C=4, G=24, n_RDB=self.n_RDB, type='N')
-        depths = [2]
         num_heads = [4]
         if 'pam' in model :
             if 'light' in model:
@@ -42,12 +41,15 @@ class Net(BaseModel):
             self.swin = SwinAttn(img_size=img_size, window_size=w_size, depths=[1], embed_dim=64, num_heads=num_heads, mlp_ratio=2)
             self.coswin = CoSwinAttn(img_size=img_size, window_size=w_size, depths=[1], embed_dim=64, num_heads=num_heads, mlp_ratio=2)
         elif any(x in model for x in ['seperate', 'independent']):
+            depths = [2]
             self.swin = SwinAttn(img_size=img_size, window_size=w_size, depths=depths, embed_dim=64, num_heads=num_heads, mlp_ratio=2)
         self.f_RDB = RDB(G0=128, C=4, G=32)
         self.CAlayer = CALayer(128)
         self.fusion = nn.Sequential(self.f_RDB, self.CAlayer, nn.Conv2d(128, 64, kernel_size=1, stride=1, padding=0, bias=True))
         self.reconstruct = RDG(G0=64, C=4, G=24, n_RDB=self.n_RDB, type='P') if 'MDB' in model else RDG(G0=64, C=4, G=24, n_RDB=self.n_RDB, type='N')
-        self.upscale = nn.Sequential(nn.Conv2d(64, 3, 3, 1, 1, bias=True), nn.Conv2d(3, 3 * upscale_factor ** 2, 1, 1, 0, bias=True), nn.PixelShuffle(upscale_factor))
+        # self.upscale = nn.Sequential(nn.Conv2d(64, 3, 3, 1, 1, bias=True), nn.Conv2d(3, 3 * upscale_factor ** 2, 1, 1, 0, bias=True), nn.PixelShuffle(upscale_factor))
+        self.upscale = nn.Sequential(nn.Conv2d(64, 64 * upscale_factor ** 2, 1, 1, 0, bias=True), nn.PixelShuffle(upscale_factor), nn.Conv2d(64, 3, 3, 1, 1, bias=True))
+
         #self.apply(self._init_weights)
 
     def forward(self, x_left, x_right, is_training = 0):
@@ -81,11 +83,10 @@ class Net(BaseModel):
                 buffer_leftT, buffer_rightT = self.pam(buffer_left, buffer_right, catfea_left, catfea_right)
         elif 'coswin' in self.model:
             buffer_leftT, buffer_rightT = self.swin(buffer_left), self.swin(buffer_right)
-            buffer_leftT, buffer_rightT = self.coswin(buffer_leftT, buffer_rightT, d_left, d_right)
-            # if 'coswin_wo_d' in self.model:
-            #     buffer_leftT, buffer_rightT = self.coswin(buffer_leftT, buffer_rightT)
-            # else:
-            #     buffer_leftT, buffer_rightT = self.coswin(buffer_leftT, buffer_rightT, d_left, d_right)            
+            if 'coswin_wo_d' in self.model:
+                buffer_leftT, buffer_rightT = self.coswin(buffer_leftT, buffer_rightT)
+            else:
+                buffer_leftT, buffer_rightT = self.coswin(buffer_leftT, buffer_rightT, d_left, d_right)            
         elif any(x in self.model for x in ['seperate', 'independent']):
             buffer_leftT, buffer_rightT = self.swin(buffer_left), self.swin(buffer_right)
         if 'mine_late_fusion' in self.model:
@@ -113,13 +114,12 @@ class Net(BaseModel):
         if 'pam' in self.model:
             flop += self.pam.flop(H, W)
         elif 'coswin' in self.model:
-            flop += self.swin.flops(H, W) + self.coswin.flops(H, W)
+            flop += 2 * self.swin.flops(H, W) + self.coswin.flops(H, W)
         elif any(x in self.model for x in ['seperate', 'independent']):
             flop += self.swin.flops(H, W)
         flop += 2 * (self.f_RDB.flop(N) + self.CAlayer.flop(N) + N * (128 + 1) * 64)
         flop += 2 * self.reconstruct.flop(N)
-        flop += 2 * (N * (64 + 1) * 64 * (self.upscale_factor ** 2) + N * 3 * (64 * 9 + 1))
-        flop += 2 * (N * (64 + 1) * 64 * (self.upscale_factor ** 2))
+        flop += 2 * (N * (64 + 1) * 64 * (self.upscale_factor ** 2) + N * (self.upscale_factor ** 2) * (64 * 9 + 1) * 3)
         return flop
 
 
@@ -384,47 +384,9 @@ class PAM(nn.Module):
         flop += 2 * self.rb.flop(N)
         flop += 2 * N * self.channel * (self.n_RDB * self.channel + 1) * (1/4)
         flop += H * (W**2) * self.channel
-        # flop += 2 * H * W
         flop += 2 * H * (W**2) * self.channel
         return flop
 
-
-class SelfAttn(nn.Module):
-
-    def __init__(self, channels, w_size):
-        super(SelfAttn, self).__init__()
-        self.channel = channels
-        self.w_size = w_size
-        self.qkv = nn.Conv2d(channels, 3 * channels, 1, 1, 0, bias=True)
-        self.softmax = nn.Softmax(-1)
-        self.bn = nn.BatchNorm2d(channels)
-
-    def forward(self, x):
-        w_size = self.w_size
-        b, c, h, w = x.shape
-        qkv = self.qkv(self.bn(x)).reshape(b, 3, c, h, w).permute(1, 0, 2, 3, 4)
-        Q , K , V = qkv[0], qkv[1], qkv[2]
-        score = torch.bmm(Q.permute(0, 2, 3, 1).contiguous().view(-1, w_size, c),                    # (B*H) * Wl * C
-                          K.permute(0, 2, 1, 3).contiguous().view(-1, c, w_size))                    # (B*H) * C * Wr
-        # (B*H) * Wl * Wr
-        attn = self.softmax(score)
-        # (B*H) * Wr * Wl
-
-        xT = torch.bmm(attn, V.permute(0, 2, 3, 1).contiguous().view(-1, w_size, c)).contiguous().view(b, h, w, c).permute(0, 3, 1, 2)  # B, C0, H0, W0
-        out = xT + x
-
-        return out
-
-
-    def flop(self, H, W):
-            w_size = self.w_size
-            N = H * W
-            flop = 0
-            flop +=  N * self.channel * (self.channel + 1) * 3
-            flop += H * W//w_size * (w_size**2) * self.channel
-            # flop += 2 * H * W
-            flop +=  H * W//w_size * (w_size**2) * self.channel
-            return flop
 
 class LightPAM(nn.Module):
 
@@ -435,7 +397,6 @@ class LightPAM(nn.Module):
         self.n_RDB = n_RDB
         self.n_RDB = 1
         self.g = 1
-        # self.s_attn = SelfAttn(channels, w_size)
         self.bq = nn.Conv2d(self.n_RDB * channels, channels, 1, 1, 0, groups=self.g, bias=True)
         self.bs = nn.Conv2d(self.n_RDB * channels, channels, 1, 1, 0, groups=self.g, bias=True)
         self.softmax = nn.Softmax(-1)
@@ -445,7 +406,6 @@ class LightPAM(nn.Module):
     def __call__(self, x_left, x_right, catfea_left, catfea_right, is_training=0):
         w_size = self.w_size
         b, c0, h0, w0 = x_left.shape
-        # x_left, x_right = self.s_attn(x_left), self.s_attn(x_right)
         Q = self.bq(self.rb(self.bn(x_left)))
         b, c, h, w = Q.shape
         Q = Q - torch.mean(Q, 3).unsqueeze(3).repeat(1, 1, 1, w)
@@ -523,7 +483,7 @@ if __name__ == "__main__":
     # from StreoSwinSR import CoSwinAttn
     # from SwinTransformer import SwinAttn
     H, W, C = 360, 640, 3
-    net = Net(upscale_factor=4, model='MDB_coswin', img_size=tuple([H, W]), input_channel=C, w_size=15).cuda()
+    net = Net(upscale_factor=4, model='MDB_pam', img_size=tuple([H, W]), input_channel=C, w_size=15).cuda()
     starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
     net.train(False)
     total = sum([param.nelement() for param in net.parameters()])

profiler.py
@@ -2,40 +2,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from extra.tritanlayernorm import LayerNorm
-starter, ender = torch.cuda.Event(
-    enable_timing=True), torch.cuda.Event(enable_timing=True)
+from models.model import Net
+from model_zoo.PASSRnet import PASSRnet
 
-cv = nn.Conv2d(64, 64, 1, 1).cuda()
-x = torch.randn(1 , 64, 32 , 96).cuda()
-with torch.no_grad():
-    for _ in range(100):
-        starter.record()
-        _ = cv(x)
-        ender.record()
-        torch.cuda.synchronize()
-        print('conv time:', starter.elapsed_time(ender) / 1000)
 
-    for _ in range(100):
-        starter.record()
-        q = x.view(1, 64, 4, 8, 12, 8).permute(0, 2, 4, 3, 5, 1).contiguous().view(1*48, 64, 64)
-        v = x.view(1, 64, 4, 8, 12, 8).permute(0, 2, 4, 3, 5, 1).contiguous().view(1*48, 64, 64)
-        ender.record()
-        torch.cuda.synchronize()
-        print('matmul time:', starter.elapsed_time(ender) / 1000)
-        attn = q @ v
+if __name__ == "__main__":
+    # from utils import disparity_alignment
+    # from StreoSwinSR import CoSwinAttn
+    # from SwinTransformer import SwinAttn
+    model_name = 'PASSRnet'
+    H, W, C = 360, 640, 3
+    upscale_factor = 4
+    if model_name == 'mine':
+        net = Net(upscale_factor=4, model='MDB_coswin', img_size=tuple(
+            [H, W]), input_channel=C, w_size=15).cuda()
+    elif model_name == 'PASSRnet':
+        net = PASSRnet(upscale_factor).cuda()
+    
+    starter, ender = torch.cuda.Event(
+        enable_timing=True), torch.cuda.Event(enable_timing=True)
+    net.train(False)
+    total = sum([param.nelement() for param in net.parameters()])
+    print('   Number of params: %.2fM' % (total / 1e6))
+    print('   FLOPS: %.2fG' % (net.flop(H, W) / 1e9))
+    x = torch.clamp(torch.randn((1, 3, H, W)), min=0.0).cuda()
+    exc_time = 0.0
+    n_itr = 10
+    with torch.no_grad():
+        for _ in range(10):
+            _, _ = net(x, x, 0)
+        for _ in range(n_itr):
+            starter.record()
+            _, _ = net(x, x, 0)
+            ender.record()
+            torch.cuda.synchronize()
+            elps = starter.elapsed_time(ender)
+            exc_time += elps
+            print('################## total: ', elps /
+                  1000, ' #######################')
 
-    w_shape = (64)
-    weight = torch.rand(w_shape, dtype=torch.float, device='cuda', requires_grad=True)
-    bias = torch.rand(w_shape, dtype=torch.float, device='cuda', requires_grad=True)
-    layer_norm = LayerNorm.apply
-    # forward pass
-    x = torch.randn(48 , 64, 64).cuda()
-    norm = nn.LayerNorm(64).cuda()
-    for _ in range(100):
-        starter.record()
-        # _ = bnorm(x)
-        y_tri = layer_norm(x, w_shape, weight, bias, 1e-5)
-        ender.record()
-        torch.cuda.synchronize()
-        print('norm time:', starter.elapsed_time(ender) / 1000)
-
+    print('exec time: ', exc_time / n_itr / 1000)